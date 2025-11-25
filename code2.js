e979f7a793459c46441913f5c5d3b6fc23c19c6e
Mena <ISHEPHERD94610.com@outlook.com>
2025-11-25 04:27:31 -0500

    Update code2.js

--- code2.js
+++ code2.js
@@ -2,6 +2,38 @@
 
 function validateInputs() { 
 
-  // Obatain the values from form 
+  // Obtain the values from form 
 
-  let firstName = document.getElementById
+  let firstName = document.getElementById("firstName").value.trim();
+
+  let lastName = document.getElementById("lastName").value.trim();
+
+  let zip = document.getElementById("zip").value.trim();
+
+  // Using first + last name 
+
+  let fullName = firstName + " " + lastName;
+
+  // Name length 
+
+  if (fullName.length > 20) {
+
+    document.getElementById("message").innerHTML = "Alert: Full name must be less than 20 characters.";
+
+    return;
+
+  }
+
+  // Using zip code: 5 digits
+
+  if (!/^\d{5}$/.test(zip) ) {
+
+    document.getElementById("message").innerHTML = "Alert: Zip code must be 5 digits.";
+
+    return;
+
+  }
+
+  // If inputs are valid, showing untold message
+
+  document.getElementById("message").innerHTML = "Horray, " + fullName + "! Here's the untold message: <strong>Cybersecurity is knowledge, wear it as armor!</strong>";
