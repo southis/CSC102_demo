6e1fd2b3cc6edb0fd41c7fc15508aac5f4379833
Mena <ISHEPHERD94610.com@outlook.com>
2025-11-30 23:20:32 -0500

    Update code3.js

--- code3.js
+++ code3.js
@@ -1,1 +1,89 @@
+//
+
+// Global for setInterval ID
+
+This allows us to stop the movement later 
+
+// 
+
+let moveInterval;
+
+//
+
+// Function: startMove()
+
+// Goal: Start the meme movement & 
+
+// disable Start button, enable Stop button
+
+//
+
+function startMove () {
+
+// Disable starting point, enable stop
+
+document.getElementById("startBtn").disabled = true;
+
+document.getElementById("stopBtn").diabled = false;
+
+// Call function constantly every 200ms to move meme image
+
+moveInterval = setInterval(moveMeme, 200);
+
+}
+
+//
+
+// Function: stopMove ()
+
+// Goal: Stop movement &
+
+// enable start button, disable stop button
+
+//
+
+function stopMove() {
+
+  // Stop the setInterval loop
+
+  clearInterval(moveInterval);
+
+  // Enable the Start again, disbale Stop
+
+  document.getElementById("startBtn").diabled = false;
+
+  document.getElementById("stopBtn").diabled = true;
+
+}
+
+//
+
+// Function: moveMeme()
+
+// Goal: Place the meme on the page 
+
+//
+
+function moveMeme () {
+
+  // Selecting meme by ID
+
+  let meme = document.getElementById("memeImage");
+
+  // Created a random X/Y position inside the window
+
+  let randomX = Math.floor(Math.random() * 500); 
+  
+  let randomY = Math.floor(Math.random() * 500);
+
+  // Applied a brand new position using CSS
+
+  meme.style.position = "relative";
+
+  meme.style.left = randomX + "px";
+
+  meme.style.top = randomY + "px";
+
+}
+
 
